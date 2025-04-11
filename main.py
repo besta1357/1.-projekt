1f12c0a978b7ae3ae06b025c0002a621168028f3
besta1357 <psebest11@gmail.com>
2025-04-11 22:39:04 +0200

    v2

--- main.py
+++ main.py
@@ -41,8 +41,9 @@ pocet_textu = (len(TEXTS))
 
 uzivatel = input("username: ")
 heslo = input("pasword: ")
+
 if heslo.isnumeric():
-  heslo = int(heslo)
+  heslo = int(heslo)  
 if uzivatel in registrovani_uzivatele and heslo == registrovani_uzivatele[uzivatel]:
   print(cara)
   print(f"Welcome to the app, {uzivatel} \nWe have {pocet_textu} texts to be analyzed. ")
@@ -53,7 +54,7 @@ if uzivatel in registrovani_uzivatele and heslo == registrovani_uzivatele[uzivat
      if vyber_textu < 1 or vyber_textu > pocet_textu:
           print("Invalid number, terminating the program...")
           exit()
-          print(cara)                 
+          print(cara)               
   else:
       print("Invalid input, terminating the program...")
       exit()
@@ -62,6 +63,7 @@ if uzivatel in registrovani_uzivatele and heslo == registrovani_uzivatele[uzivat
 else:
   print("unregistered user, terminating the program...")
   exit()
+
 vycistena_slova = [slovo.strip(",.:;'") for slovo in TEXTS[vyber_textu - 1].split()]
 pocet_slov = len(vycistena_slova)
 print(f"Text number {vyber_textu} contains {pocet_slov} words.") 
@@ -80,22 +82,17 @@ suma_cisel = sum(cisla_v_textu)
 #pocet_slov = len(vycistena_slova)         
 for slova in TEXTS[vyber_textu - 1].split():
     ciste_slovo = slova.strip(",.:;'")
-    vycistena_slova.append(ciste_slovo)
-    #num = [int(x) for x in TEXTS[0].split() if x.isdigit()]
+    vycistena_slova.append(ciste_slovo)    
     if slova.islower():
-        pocet_malych_slov += 1
-      #print(f"There are {pocet_malych_slov} lowercase words.")
+        pocet_malych_slov += 1      
     elif slova.isupper():
-        pocet_velkych_slov += 1
-       #print(f"There are {pocet_velkych_slov} uppercase words.")
+        pocet_velkych_slov += 1       
     elif slova.isnumeric():
-        pocet_cisel += 1
-       #print(f"There are {pocet_cisel} numeric strings.")    
+        pocet_cisel += 1          
     for pismena in slova:
         break        
     if pismena.isupper():
-       pocet_slov_s_velkym_pismenom += 1
-      #print(f"There are {pocet_slov_s_velkym_pismenom} titlecase words.")
+       pocet_slov_s_velkym_pismenom += 1      
 
 print(f"There are {pocet_malych_slov} lowercase words.")
 print(f"There are {pocet_velkych_slov} uppercase words.")
@@ -103,13 +100,13 @@ print(f"There are {pocet_slov_s_velkym_pismenom - pocet_velkych_slov} titlecase 
 print(f"There are {pocet_cisel} numeric strings.")
 print(f"The sum of all the numbers {suma_cisel}")
 
-for slova in TEXTS[vyber_textu - 1].split():
+for slova in TEXTS[0].split():
     ciste_slovo = slova.strip(",.:;'")
     vycistena_slova.append(ciste_slovo)
     pocet_pismen_ve_slovech = len(ciste_slovo)
     pismena_ve_slovech.append(pocet_pismen_ve_slovech)
-#print(pismena_ve_slovech)
-nejdelsi_slovo = int(max(pismena_ve_slovech))
+
+#nejdejelsi_slovo = int(max(pismena_ve_slovech))
 
 vyskyt_slov = {}
 for cetnost in pismena_ve_slovech:
@@ -118,11 +115,12 @@ for cetnost in pismena_ve_slovech:
     else:
         vyskyt_slov[cetnost] = vyskyt_slov[cetnost] + 1
 serazeny_vyskyt_slov = dict(sorted(vyskyt_slov.items()))
-#print(vyskyt_slov)
-#print(serazeny_vyskyt_slov)
+nejvyssi_cetnost = max(serazeny_vyskyt_slov.values())
 print(cara)
-print("LEN|".center(5) + "OCCURENCES".center(15) + "| NR.".center(0))
+print("LEN |".center(5) + "OCCURENCES".center(15) + "| NR.".center(10))
 print(cara)
+
 for delka_slova in serazeny_vyskyt_slov:
-    #print(f"{delka_slova}|".center(8) + ((serazeny_vyskyt_slov[delka_slova])*"*") + f"|{serazeny_vyskyt_slov[delka_slova]}".center(10))
-    print(f"{delka_slova: ^4}|{serazeny_vyskyt_slov[delka_slova]*"*": ^4}" + f"{" ": ^1}" + f"{serazeny_vyskyt_slov[delka_slova]}")
+    print(f"{delka_slova: ^4}|{serazeny_vyskyt_slov[delka_slova]*"*": ^1}" + 
+          f"{" "*((nejvyssi_cetnost - serazeny_vyskyt_slov[delka_slova])+8)}" + 
+          f"|{serazeny_vyskyt_slov[delka_slova]}")
